dfbefe18d05e846b20794858771485f1b841efc6
rashmi pandey <rashmipandey192@gmail.com>
2017-08-03 16:15:09 +0530

    new practise file created

---
+++ hello.py
@@ -0,0 +1,10 @@
+print"hello friends!"
+a="\nRASHMI"
+b="PANDEY"
+print a+b
+
+a=4587
+b=98600
+print "\nAddition of a and b:-"
+print a+b
+
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print"let's get started!"
+print "let's get started!"
+print "welcome to spy chat application..."
+raw_input("please provide your name here..")
