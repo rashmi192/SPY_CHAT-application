ac9410bc2e1207d20cfae330e3e3950a6fc5bfb2
rashmi pandey <rashmipandey192@gmail.com>
2017-08-08 11:09:57 +0530

    project work

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 print "               WELCOME TO SPY  \n                   CHAT"
-spy_name=raw_input("Please provide your name here--")
-if len(spy_name)>0:
-
+spy_name = raw_input("Please provide your name here--")
+if len(spy_name) > 0:
     spy_salutation = raw_input("What should we call you (MR or MISS)?-- ")
-if len(spy_salutation) > 0:
-
-    print"Welcome " + spy_salutation + " " + spy_name + " glad to have you back with us ! "
+    spy_name = spy_salutation + " " + spy_name
+    print"Welcome " + " " + spy_name + " glad to have you back with us ! "
+    print "Alright"+ " "+spy_name+" " +"i want to know little more about you"
 else:
-    print "Sorry,name cannot be blank!"
+    print"name cannot be blank"
+#conatena"tion of salutation and name
 
-
-#conatenation of salutation and name
-
-#spy_name =spy_salutation + " " + spy_name (this is called reassignment)
